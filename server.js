18f9e95395850a38fcbaf830a5f239b08ee6a16d
crodrigo2003 <crodrigo@50wise.com>
2017-11-10 19:22:05 +0100

    Add project page

--- server.js
+++ server.js
@@ -45,6 +45,11 @@ app.get('/', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects'
+    });
+});
 
 
 app.get('/about', (req, res) => {
